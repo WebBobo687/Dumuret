d6dd3bb412c906bad9886b75c9ce14bddf0218bf
Elias <elias.dupin-gilet@codewave.nc>
2023-01-07 16:05:55 +1100

    updateCarteCSS

--- index.js
+++ index.js
@@ -14,3 +14,34 @@ function constructionCard() {
         confirmButtonColor: '#00b3fa'
     })
 }
+
+function habillageCard() {
+    Swal.fire({
+        title: '<strong>Habillage</strong>',
+        html:
+        '- Pose de carrelage,</br>' +
+        '- Pose de parquet,</br>' +
+        '- Installation de fenêtres et de portes,</br>' +
+        '- Pose de plaques de plâtre,</br>' +
+        '- Peinture,</br>' +
+        '- Revêtement piscine',
+        icon: 'info',
+        confirmButtonText: 'Ok',
+        confirmButtonColor: '#00b3fa'
+    })
+}
+
+function renovationCard() {
+    Swal.fire({
+        title: '<strong>Rénovation</strong>',
+        html:
+        '<strong>Rénovation légère :</strong>  travaux de petite envergure, qui visent à apporter des améliorations esthétiques ou fonctionnelles à un bâtiment sans toucher à sa structure. Cela peut inclure la peinture, le remplacement de revêtements de sol ou de carrelage, l\'installation de nouveaux équipements ou la pose de cloisons.</br>' +
+        '</br>' +
+        '<strong>Rénovation lourde :</strong> travaux de plus grande envergure, qui impliquent des modifications plus importantes à la structure d\'un bâtiment. Cela peut inclure la réfection de toitures, la modification de portes et de fenêtres, l\'ajout de pièces ou la réorganisation des espaces intérieurs.</br>' +
+        '</br>' +
+        '<strong>Rénovation de façade :</strong> travaux visant à améliorer l\'apparence extérieure d\'un bâtiment, en réparant ou en ravalant sa façade. Cela peut inclure la réparation de fissures, le remplacement de matériaux dégradés ou la peinture de la façade.',
+        icon: 'info',
+        confirmButtonText: 'Ok',
+        confirmButtonColor: '#00b3fa'
+    })
+}
